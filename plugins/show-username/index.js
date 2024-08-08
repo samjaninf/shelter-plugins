a2286143d21dcce70c7758e823a1ff2bce947bc4
Richard Rattary <narutoksan@gmail.com>
2024-08-07 22:46:53 -0400

    Update show-usernames from ioj4 including parentheses

--- plugins/show-username/index.js
+++ plugins/show-username/index.js
@@ -1,71 +1,51 @@
 const {
-	flux: {
-		dispatcher,
-		stores: {
-			GuildMemberStore,
-			ChannelStore,
-			SelectedChannelStore,
-			RelationshipStore,
-		},
-	},
-	util: { getFiber, reactFiberWalker },
-	observeDom,
+  plugin: { scoped, store },
+  util: { getFiber, reactFiberWalker }
 } = shelter;
 
-function addUsername(e) {
-	if (e?.dataset?.ysink_su) return;
-	e.dataset.ysink_su = true;
+const USERNAME_QUERY = '[id^="message-username-"] > [class^="username"]';
 
-	const msg = reactFiberWalker(getFiber(e), "message", true).pendingProps
-		?.message;
-
-	const authorUsername = msg.author?.username;
-	const authorId = msg?.author?.id;
-
-	const { type, guild_id: guildId } = ChannelStore.getChannel(msg?.channel_id);
-
-	// type = 0: Guild, 1: DM
-	const nick = type
-		? RelationshipStore.getNickname(authorId)
-		: GuildMemberStore.getNick(guildId, authorId);
-
-	if (!nick || !authorUsername) return;
-
-	e.firstElementChild.textContent += ` (${authorUsername})`;
+export function forceAddUsernames() {
+  for (const e of document.querySelectorAll(USERNAME_QUERY)) {
+      addUsername(e, true);
+  }
 }
 
-// MESSAGE_CREATE: a new message is sent
-// CHANNEL_SELECT: the user switches servers
-// LOAD_MESSAGES_SUCCESS: new messages in viewport
-// UPDATE_CHANNEL_DIMENSIONS: the user scrolls back down perhaps
-const TRIGGERS = [
-	"MESSAGE_CREATE",
-	"CHANNEL_SELECT",
-	"LOAD_MESSAGES_SUCCESS",
-	"UPDATE_CHANNEL_DIMENSIONS",
-];
+function addUsername(e, overwrite = false) {
+  if (e.querySelector(".shelter-su") && !overwrite) return;
 
-function onDispatch(payload) {
-	// ignore MESSAGE_CREATEs from other channels
-	if (
-		payload.type === "MESSAGE_CREATE" &&
-		payload.channelId !== SelectedChannelStore.getChannelId()
-	)
-		return;
+  const props = reactFiberWalker(getFiber(e), "message", true)?.pendingProps;
+  if (!props?.author || !props?.message) return;
 
-	const unObserve = observeDom("[id^=message-username-]", (element) => {
-		unObserve();
-		addUsername(element);
-	});
+  const { nick } = props.author;
+  const { username } = props.message.author;
 
-	// don't leave this forever, just in case!
-	setTimeout(unObserve, 500);
+  const style =
+      "font-weight: 600;border-radius: 4px;padding: 0 4px;background: var(--background-secondary);";
+  const usernameElement = (
+      <span style={style} className={"shelter-su"}>
+          ({username})
+      </span>
+  );
+
+  // const appendNick = nick && !store.usernamesOnly && username !== nick;
+  const appendNick = nick && !store.usernamesOnly;
+  e.textContent = appendNick ? `${nick} ` : ``;
+  e.append(usernameElement);
 }
 
 export function onLoad() {
-	for (const t of TRIGGERS) dispatcher.subscribe(t, onDispatch);
+  store.usernamesOnly ??= false;
+  // apply on usernames that are already in the DOM
+  forceAddUsernames();
+
+  // using a long living observeDom here because awaiting dispatches is a bit slower and
+  // adds excessive complexity for what it's worth
+  scoped.observeDom(USERNAME_QUERY, (e) => {
+      queueMicrotask(() => {
+          addUsername(e);
+      });
+  });
 }
 
-export function onUnload() {
-	for (const t of TRIGGERS) dispatcher.unsubscribe(t, onDispatch);
-}
+export { default as settings } from "./settings";
