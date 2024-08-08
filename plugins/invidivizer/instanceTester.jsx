8a6323365a13c4b29e0a6a9f2a680893137a0663
Richard Rattary <narutoksan@gmail.com>
2024-08-08 01:41:45 -0400

    update invidivizer and show usernames

--- plugins/invidivizer/instanceTester.jsx
+++ plugins/invidivizer/instanceTester.jsx
@@ -1,28 +1,30 @@
 export async function updateFastestInstance() {
-  const response = await fetch("https://api.invidious.io/instances.json?pretty=0&sort_by=type,users");
-  const json = await response.json();
-  const instances = json.map((instance) => instance[1].uri);
+	const response = await fetch(
+		"https://api.invidious.io/instances.json?pretty=0&sort_by=type,users",
+	);
+	const json = await response.json();
+	const instances = json.map((instance) => instance[1].uri);
 
-  console.log("Testing Invidious instances...");
-  let prevBestTime = Infinity;
-  let prevBestInstance = null;
-  for (const instance of instances) {
-      if (!instance.includes("https")) continue;
-      const start = performance.now();
-      try {
-          await fetch(instance, {
-              mode: 'no-cors'
-          });
-      } catch (e) {}
-      const end = performance.now();
-      const time = end - start;
-      console.log(instance, time);
-      if (time < prevBestTime) {
-          prevBestTime = time;
-          prevBestInstance = instance;
-      }
-  }
+	console.log("Testing Invidious instances...");
+	let prevBestTime = Infinity;
+	let prevBestInstance = null;
+	for (const instance of instances) {
+		if (!instance.includes("https")) continue;
+		const start = performance.now();
+		try {
+			await fetch(instance, {
+				mode: "no-cors",
+			});
+		} catch (e) {}
+		const end = performance.now();
+		const time = end - start;
+		console.log(instance, time);
+		if (time < prevBestTime) {
+			prevBestTime = time;
+			prevBestInstance = instance;
+		}
+	}
 
-  console.log("Fastest instance:", prevBestInstance, prevBestTime);
-  return prevBestInstance;
+	console.log("Fastest instance:", prevBestInstance, prevBestTime);
+	return prevBestInstance;
 }
