cc78db3fe1f6f38a2f41591c255fb97ff72a100f
Hazel Atkinson <yellowsink@riseup.net>
2026-01-25 15:23:26 +0000

    bump lune ssg

--- lune.config.js
+++ lune.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "@uwu/lune";
 
 export default defineConfig({
-	ssg: { repo_name: "Hazel's plugins" },
+	ssg: {
+		repo_name: "Hazel's plugins",
+		base_url: "https://yellowsink.github.io/shelter-plugins",
+	},
 });
